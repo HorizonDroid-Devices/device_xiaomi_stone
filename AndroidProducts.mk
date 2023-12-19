7a556276226a701dee94c9571928ebaea15a2691
Drpto <dr.juniornoob@gmail.com>
2023-12-19 23:25:55 +0530

    stone: Go with HorizonDroid

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_stone.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_stone-user \
+    aosp_stone-userdebug \
+    aosp_stone-eng
---
+++ BoardConfig.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/stone
+
+# Inherit from sm6375-common
+include device/xiaomi/sm6375-common/BoardConfigCommon.mk
+
+# Board Info
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+
+# Display
+TARGET_SCREEN_DENSITY := 440
+
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_stone
+TARGET_RECOVERY_DEVICE_MODULES := init_stone
+
+# OTA Assert
+TARGET_OTA_ASSERT_DEVICE := moonstone,sunstone,stone
+
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Inherit from the proprietary version
+include vendor/xiaomi/stone/BoardConfigVendor.mk
